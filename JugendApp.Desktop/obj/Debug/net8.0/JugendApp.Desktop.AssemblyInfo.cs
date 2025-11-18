82dc120cb0e9fc1fd9dcf57a2b8d4691eee0254a
JLionSoft <josua@l-en.de>
2025-11-18 14:41:49 +0100

    nothing

--- JugendApp.Desktop/obj/Debug/net8.0/JugendApp.Desktop.AssemblyInfo.cs
+++ JugendApp.Desktop/obj/Debug/net8.0/JugendApp.Desktop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JugendApp.Desktop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a07c31d2d2a9084340ac28de14963cfdb8d75567")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba22fcd90a79abcbcc8bb7950e56859249839fc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("JugendApp.Desktop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JugendApp.Desktop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- JugendApp.Web/obj/Debug/net8.0/JugendApp.Web.AssemblyInfo.cs
+++ JugendApp.Web/obj/Debug/net8.0/JugendApp.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JugendApp.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a07c31d2d2a9084340ac28de14963cfdb8d75567")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba22fcd90a79abcbcc8bb7950e56859249839fc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("JugendApp.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JugendApp.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
